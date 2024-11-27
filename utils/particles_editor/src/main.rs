f024c31c8d030b4155df54fc3253c336ce105c7b
Bryan McCarthy <bryanmccarthy2@gmail.com>
2024-11-27 18:03:48 +0000

    presets mod

--- utils/particles_editor/src/main.rs
+++ utils/particles_editor/src/main.rs
@@ -2,6 +2,8 @@ use macroquad::prelude::*;
 use macroquad::ui::{self as ui};
 use macroquad_particles::{self as particles};
 
+mod presets;
+
 struct ParticlesEditor {
     emitter: particles::Emitter,
     coords: Vec2,
@@ -10,19 +12,6 @@ struct ParticlesEditor {
 impl ParticlesEditor {
     fn new() -> Self {
         let emitter = particles::Emitter::new(particles::EmitterConfig {
-            local_coords: false,
-            one_shot: false,
-            emitting: true,
-            lifetime: 0.4,
-            lifetime_randomness: 0.5,
-            explosiveness: 0.1,
-            initial_direction: Vec2::new(0.0, 1.0),
-            initial_direction_spread: 2.0,
-            initial_velocity: 400.0,
-            initial_velocity_randomness: 0.8,
-            size: 10.0,
-            size_randomness: 0.3,
-            atlas: Some(particles::AtlasConfig::new(5, 1, 0..)),
             ..Default::default()
         });
 
@@ -38,14 +27,21 @@ impl ParticlesEditor {
 
 #[macroquad::main("particle editor")]
 async fn main() {
-    
     let mut editor = ParticlesEditor::new();
 
     loop {
         clear_background(BLACK);
 
-        if ui::root_ui().button(None, "PUSH") {
-            editor.emitter.config.size = 30.0;
+        if ui::root_ui().button(None, "Smoke") {
+            editor.emitter.config = presets::smoke();
+        }
+
+        if ui::root_ui().button(None, "Fire") {
+            editor.emitter.config = presets::fire();
+        }
+
+        if ui::root_ui().button(None, "Explosion") {
+            editor.emitter.config = presets::explosion();
         }
 
         editor.draw_emitter();
